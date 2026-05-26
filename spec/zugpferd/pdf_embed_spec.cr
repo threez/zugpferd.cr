5085bb39572ef4597368d1b7d18c82adf365f42c
Vincent Landgraf <vincent@landgrafx.de>
2026-05-26 09:12:11 +0200

    add PdfEmbed for PDF/A-3b ZUGFeRD output

---
+++ spec/zugpferd/pdf_embed_spec.cr
@@ -0,0 +1,19 @@
+require "../spec_helper"
+require "../../src/zugpferd/pdf_embed"
+
+describe Zugpferd::PdfEmbed do
+  it "creates a PDF/A-3b file with the ZUGFeRD XML embedded" do
+    xml = SpecHelper.minimal_invoice(Zugpferd::Profile::EN16931).to_xml
+    tmp = File.tempfile("zugferd-embed-spec", ".pdf")
+    begin
+      Zugpferd::PdfEmbed.create(xml, tmp.path) do |doc|
+        doc.add_page
+      end
+      bytes = File.read(tmp.path)
+      bytes[0, 4].should eq("%PDF")
+      bytes.should contain("factur-x.xml")
+    ensure
+      tmp.delete
+    end
+  end
+end
